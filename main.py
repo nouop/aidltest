b1a272bdb1e2ac60102f6c3a654772aff2b89f85
nicemark <nicemark@noreply.gitcode.com>
2025-04-19 21:25:34 +0800

    更新主程序以支持选择AI和语言，并修复工作日志保存功能

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ ainame = ["Qwen/Qwen2.5-Coder-7B-Instruct","Qwen/QwQ-32B","deepseek-ai/DeepSeek-
 ainamenum = 0
 prompt = "你是一个数学专家，请根据题目使用中文给出答案"
 qp = "使用中文思考并回答问题："
-# prompt = "you are a math expert, please answer the question using English."
-# qp = "use English to think and answer the question:"
+
 worklog = {}
+
 
 
 def save_to_json(data, filename):
@@ -30,24 +30,51 @@ def save_to_json(data, filename):
 
 
 def main():
-    worklog[ainame[ainamenum]] = {}
-    for i in range(10):
-        # question_ = read_question("./data/2010-2022_Math_I_MCQs.json", i)
-        question_ = read_question_h(i)
-        print(question_)
-        response = silconflow_api(qp+question_[0], prompt, ainame[ainamenum])
-        ai_answear = silconflow_api(response, "你是作答筛选机器人，旨在从文本中去除过程，筛选返回题目答案，只能回答字母或数字！！!（如A,B,C,D,92,68等）", "deepseek-ai/DeepSeek-V3")
-        print(response)
-        print(ai_answear)
-        if ai_answear.strip() == question_[1]:
-            worklog[ainame[ainamenum]][str(i+1)] = True
-            print("正确")
-        else:
-            worklog[ainame[ainamenum]][str(i+1)] = False
-            print("错误")
+    for m in ainame:
+        print(str(ainame.index(m))+":"+m)
+    ainamenum = input("请选择ai(输入索引)：")
+    if ainamenum != None:
+        ainamenum = int(ainamenum)
+    lc = input("请选择语言(c/e)：")
+    if lc == "e":
+        prompt = "you are a math expert, please answer the question using English."
+        qp = "use English to think and answer the question:"
+    qn = input("请选择题目数量（1，200）：")
+    qn = int(qn)
+    if not (qn <= 200 and qn >= 1):
+        qn = 1
+    print("你选择的AI是：", ainame[ainamenum])
+    print("你选择的语言是：", lc)
+    print("你选择的题目数量是：", qn)
+    check = input("是否开始？（y/n）")
+    if check == "y":
+        try:
+            worklog[ainame[ainamenum]] = {}
+            for i in range(qn):
+                # question_ = read_question("./data/2010-2022_Math_I_MCQs.json", i)
+                question_ = read_question_h(i)
+                print(question_)
+                response = silconflow_api(qp+question_[0], prompt, ainame[ainamenum])
+                ai_answear = silconflow_api(response, "你是作答筛选机器人，旨在从文本中去除过程，筛选返回题目答案，只能回答字母或数字！！!（如A,B,C,D,92,68等）", "deepseek-ai/DeepSeek-V3")
+                print(response)
+                print(ai_answear)
+                if ai_answear.strip() == question_[1]:
+                    worklog[ainame[ainamenum]][str(i+1)] = True
+                    print("正确")
+                else:
+                    worklog[ainame[ainamenum]][str(i+1)] = False
+                    print("错误")
 
-    save_to_json(worklog, "worklog.json")
-    print(worklog)
+                save_to_json(worklog, "worklog.json")
+                print(worklog)
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    
+    else:
+        print("已取消")
+
+
 
 
 if __name__ == "__main__":
